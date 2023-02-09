973ac65c46d29d5446f9aeb68b3937422b22a662
Cleiton Salvagni <cleiton_salvagni@estudante.sc.senai.br>
2023-02-09 17:57:04 -0300

    feat: initialized project and added template admin

---
+++ application/views/home/index.php
@@ -0,0 +1,7 @@
+<?php $this->load->view("layout/sidebar"); ?>
+<div id="content">
+  <?php $this->load->view("layout/navbar") ?>
+  <div class="container-fluid">
+    <h1 class="h3 mb-4 text-gray-800">Home</h1>
+  </div>
+</div>
