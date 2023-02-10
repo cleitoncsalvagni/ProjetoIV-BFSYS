be65ecaad2abd12aff5ae36f3822bd8ccd79b906
Cleiton Salvagni <cleiton_salvagni@estudante.sc.senai.br>
2023-02-10 15:02:16 -0300

    fix: script/link initialization

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -2,7 +2,7 @@
   <div class="container my-auto">
     <div class="copyright text-center my-auto">
       <!-- <span>Copyright &copy; BFSYS <?php echo date('Y') ?></span> -->
-      <span>Copyright &copy; BFSYS <?php echo date('Y') ?>&nbsp; | Por: Cleiton Salvagni</span>
+      <span>Copyright &copy; BFSYS <?php echo date('Y') ?> | Por Cleiton Salvagni</span>
     </div>
   </div>
 </footer>
@@ -25,7 +25,7 @@
           <span aria-hidden="true">×</span>
         </button>
       </div>
-      <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+      <div class="modal-body">Select "Logout" below if you are reFady to end your current session.</div>
       <div class="modal-footer">
         <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
         <a class="btn btn-primary" href="login.html">Logout</a>
@@ -34,12 +34,13 @@
   </div>
 </div>
 
-<script src="public/vendor/jquery/jquery.min.js"></script>
-<script src="public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo base_url("public/vendor/jquery/jquery.min.js"); ?>"></script>
 
-<script src="public/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="<?php echo base_url("public/vendor/bootstrap/js/bootstrap.bundle.min.js"); ?>"></script>
 
-<script src="public/js/sb-admin-2.min.js"></script>
+<script src="<?php echo base_url("public/vendor/jquery-easing/jquery.easing.min.js"); ?>"></script>
+
+<script src="<?php echo base_url("public/js/sb-admin-2.min.js") ?>"></script>
 
 
 <?php if (isset($scripts)) : ?>
