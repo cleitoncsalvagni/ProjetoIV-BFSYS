670ec6f7f8f442deb78c5c5ff012d8c5d05eb200
Cleiton Salvagni <cleiton_salvagni@estudante.sc.senai.br>
2023-02-10 10:44:28 -0300

    feat: created controller Usuarios

---
+++ application/controllers/Usuarios.php
@@ -0,0 +1,25 @@
+<?php
+
+defined('BASEPATH') or exit('Ação não permitida');
+
+
+class Usuarios extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+
+        $data = array(
+            'usuarios' => $this->ion_auth->users()->result(),
+
+        );
+
+        $this->load->view('layout/header', $data);
+        $this->load->view('usuarios/index', $data);
+        $this->load->view('layout/footer', $data);
+    }
+}
