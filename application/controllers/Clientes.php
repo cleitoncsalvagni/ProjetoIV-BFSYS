54396d6c6d9fec35910c7f2dd7d89244d50474a5
Cleiton Salvagni <cleiton_salvagni@estudante.sc.senai.br>
2023-02-16 17:27:02 -0300

    feat: novo cliente

--- application/controllers/Clientes.php
+++ application/controllers/Clientes.php
@@ -141,6 +141,124 @@ class Clientes extends CI_Controller
         }
     }
 
+    public function novo()
+    {
+        $cliente_tipo = $this->input->post('cliente_tipo');
+
+        $this->form_validation->set_rules('cliente_nome', '', 'trim|required|min_length[4]|max_length[45]');
+        $this->form_validation->set_rules('cliente_sobrenome', '', 'trim|required|min_length[4]|max_length[150]');
+        $this->form_validation->set_rules('cliente_data_nascimento', '', 'required');
+
+
+        if ($cliente_tipo == 1) {
+            $this->form_validation->set_rules('cliente_cpf', '', 'trim|required|min_length[10]|max_length[20]|is_unique[clientes.cliente_cpf_cnpj]|callback_check_cpf');
+        } else {
+            $this->form_validation->set_rules('cliente_cnpj', '', 'trim|required|min_length[10]|max_length[20]|is_unique[clientes.cliente_cpf_cnpj]|callback_check_cnpj');
+        }
+
+        $this->form_validation->set_rules('cliente_rg_ie', '', 'trim|max_length[20]|is_unique[clientes.cliente_rg_ie]');
+        $this->form_validation->set_rules('cliente_email', '', 'trim|valid_email|max_length[50]|is_unique[clientes.cliente_email]');
+
+
+        if ($this->input->post('cliente_telefone')) {
+            $this->form_validation->set_rules('cliente_telefone', '', 'trim|max_length[14]|is_unique[clientes.cliente_telefone]');
+        }
+
+        if ($this->input->post('cliente_celular')) {
+            $this->form_validation->set_rules('cliente_celular', '', 'trim|max_length[14]|is_unique[clientes.cliente_celular]');
+        }
+
+        $this->form_validation->set_rules('cliente_celular', '', 'trim|max_length[15]');
+        $this->form_validation->set_rules('cliente_cep', '', 'trim|required|exact_length[9]');
+        $this->form_validation->set_rules('cliente_endereco', '', 'trim|required|max_length[155]');
+        $this->form_validation->set_rules('cliente_numero_endereco', '', 'trim|max_length[20]');
+        $this->form_validation->set_rules('cliente_bairro', '', 'trim|required|max_length[45]');
+        $this->form_validation->set_rules('cliente_complemento', '', 'trim|max_length[145]');
+        $this->form_validation->set_rules('cliente_cidade', '', 'trim|required|max_length[50]');
+        $this->form_validation->set_rules('cliente_estado', '', 'trim|required|exact_length[2]');
+        $this->form_validation->set_rules('cliente_obs', '', 'max_length[400]');
+
+        if ($this->form_validation->run()) {
+            $data = elements(
+                array(
+                    'cliente_nome',
+                    'cliente_sobrenome',
+                    'cliente_data_nascimento',
+                    'cliente_rg_ie',
+                    'cliente_email',
+                    'cliente_celular',
+                    'cliente_telefone',
+                    'cliente_endereco',
+                    'cliente_numero_endereco',
+                    'cliente_complemento',
+                    'cliente_bairro',
+                    'cliente_cep',
+                    'cliente_cidade',
+                    'cliente_estado',
+                    'cliente_ativo',
+                    'cliente_obs',
+                    'cliente_tipo',
+                ),
+                $this->input->post()
+            );
+
+            if ($cliente_tipo == 1) {
+                $data['cliente_cpf_cnpj'] = $this->input->post('cliente_cpf');
+            } else {
+                $data['cliente_cpf_cnpj'] = $this->input->post('cliente_cnpj');
+            }
+
+            $data['cliente_estado'] = strtoupper($this->input->post('cliente_estado'));
+
+            $data = html_escape($data);
+
+            $this->core_model->insert('clientes', $data);
+            redirect('clientes');
+        } else {
+            $data = array(
+
+                'titulo' => 'Cadastrar Cliente',
+
+                'scripts' => array(
+                    'vendor/mask/jquery.mask.min.js',
+                    'vendor/mask/app.js',
+                    'js/clientes.js',
+                ),
+
+            );
+
+            // echo '<pre>';
+            // print_r($data['cliente']);
+            // exit();
+
+            $this->load->view('layout/header', $data);
+            $this->load->view('clientes/novo');
+            $this->load->view('layout/footer');
+        }
+    }
+
+    public function del($cliente_id = NULL)
+    {
+
+        if (!$cliente_id || !$this->ion_auth->user($cliente_id)->row()) {
+            $this->session->set_flashdata('error', 'Usuário não encontrado');
+            redirect('usuarios');
+        }
+
+        if ($this->ion_auth->is_admin($cliente_id)) {
+            $this->session->set_flashdata('error', 'O administrador não pode ser excluído!');
+            redirect('usuarios');
+        }
+
+        if ($this->ion_auth->delete_user($cliente_id)) {
+            $this->session->set_flashdata('sucesso', 'Usuário excluído com sucesso!');
+            redirect('usuarios');
+        } else {
+            $this->session->set_flashdata('error', 'Erro ao deletar usuário. Tente novamente.');
+            redirect('usuarios');
+        }
+    }
+
     public function check_email($cliente_email)
     {
         $cliente_id = $this->input->post('cliente_id');
---
+++ application/views/clientes/novo.php
@@ -0,0 +1,171 @@
+<?php $this->load->view("layout/sidebar"); ?>
+<div id="content">
+    <?php $this->load->view("layout/navbar") ?>
+    <div class="container-fluid">
+
+
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="<?php echo base_url('clientes') ?>">Clientes</a></li>
+                <li class="breadcrumb-item active" aria-current="page"><?php echo $titulo ?></li>
+            </ol>
+        </nav>
+
+        <div class="card shadow mb-4">
+            <div class="card-body">
+                <form method="POST" name="form_add">
+
+
+                    <div>
+                        <div class="custom-control custom-radio custom-control-inline mt-2">
+                            <input type="radio" id="pessoa_fisica" name="cliente_tipo" class="custom-control-input" value="1" <?php echo set_checkbox('cliente_tipo', '1') ?> checked="">
+                            <label class="custom-control-label" for="pessoa_fisica">Pessoa física</label>
+                        </div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input type="radio" id="pessoa_juridica" name="cliente_tipo" class="custom-control-input" value="2" <?php echo set_checkbox('cliente_tipo', '2') ?>>
+                            <label class="custom-control-label" for="pessoa_juridica">Pessoa jurídica</label>
+                        </div>
+                    </div>
+
+                    <fieldset class='mt-5 border p-3'>
+                        <legend class='small'><i class="fas fa-fw fa-user-tie"></i>&nbsp; Dados Pessoais</legend>
+
+                        <div class="form-group row mb-3">
+                            <div class="col-md-5">
+                                <label>Nome</label>
+                                <input type="text" class="form-control" name="cliente_nome" placeholder="Informe o nome" value="<?php echo set_value('cliente_nome'); ?>">
+                                <?php echo form_error('cliente_nome', '<small class="form-text text-danger">', '</small>') ?>
+
+                            </div>
+
+                            <div class="col-md-5">
+                                <label>Sobrenome</label>
+                                <input type="text" class="form-control form-control-user" name="cliente_sobrenome" placeholder="Informe o sobrenome" value="<?php echo set_value('cliente_sobrenome'); ?>">
+                                <?php echo form_error('cliente_sobrenome', '<small class="form-text text-danger">', '</small>') ?>
+                            </div>
+
+                            <div class="col-md-2">
+                                <label>Data de nascimento</label>
+                                <input type="date" class="form-control" name="cliente_data_nascimento" value="<?php echo set_value('cliente_data_nascimento'); ?>">
+                                <?php echo form_error('cliente_data_nascimento', '<small class="form-text text-danger">', '</small>') ?>
+                            </div>
+
+                        </div>
+
+                        <div class="form-group row mb-3">
+                            <div class='col-md-3 pessoa_fisica'>
+                                <label>CPF</label>
+                                <input type="text" class="form-control cpf" name="cliente_cpf" placeholder="Informe o CPF" value="<?php echo set_value('cliente_cpf') ?>">
+                                <?php echo form_error('cliente_cpf', '<small class="form-text text-danger">', '</small>') ?>
+                            </div>
+
+                            <div class='col-md-3 pessoa_juridica'>
+                                <label>CNPJ</label>
+                                <input type="text" class="form-control cnpj" name="cliente_cnpj" placeholder="Informe o CNPJ" value="<?php echo set_value('cliente_cnpj') ?>">
+                                <?php echo form_error('cliente_cnpj', '<small class="form-text text-danger">', '</small>') ?>
+                            </div>
+
+                            <div class="col-md-3">
+                                <label class='pessoa_fisica'>RG</label>
+                                <label class='pessoa_juridica'>Inscrição Estadual</label>
+                                <input type="text" class="form-control" name="cliente_rg_ie" value="<?php echo set_value('cliente_rg_ie'); ?>">
+                                <?php echo form_error('cliente_rg_ie', '<small class="form-text text-danger">', '</small>') ?>
+                            </div>
+
+                            <div class="col-md-6">
+                                <label>Email</label>
+                                <input type="text" class="form-control" name="cliente_email" placeholder="Informe o email" value="<?php echo set_value('cliente_email'); ?>">
+                                <?php echo form_error('cliente_email', '<small class="form-text text-danger">', '</small>') ?>
+
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-3">
+                            <div class="col-md-3">
+                                <label>Celular</label>
+                                <input type="text" class="form-control sp_celphones" name="cliente_celular" placeholder="Informe o celular" value="<?php echo set_value('cliente_celular') ?>">
+                                <?php echo form_error('cliente_celular', '<small class="form-text text-danger">', '</small>') ?>
+                            </div>
+
+                            <div class="col-md-3">
+                                <label>Telefone fixo</label>
+                                <input type="text" class="form-control phone_with_ddd" name="cliente_telefone" placeholder="Informe o telefone fixo" value="<?php echo set_value('cliente_telefone') ?>">
+                                <?php echo form_error('cliente_telefone', '<small class="form-text text-danger">', '</small>') ?>
+                            </div>
+                        </div>
+
+                    </fieldset>
+
+                    <fieldset class='mt-4 border p-3'>
+                        <legend class='small'><i class="fas fa-map-marker-alt"></i>&nbsp; Endereço</legend>
+                        <div class="form-group row mb-3">
+                            <div class="col-md-4">
+                                <label>Endereço</label>
+                                <input type="text" class="form-control" name="cliente_endereco" placeholder="Informe o endereço" value="<?php echo set_value('cliente_endereço') ?>">
+                                <?php echo form_error('cliente_endereco', '<small class="form-text text-danger">', '</small>') ?>
+                            </div>
+                            <div class="col-md-2">
+                                <label>Numero</label>
+                                <input type="text" class="form-control" name="cliente_numero_endereco" placeholder="Informe o número" value="<?php echo set_value('cliente_numero_endereco') ?>">
+                                <?php echo form_error('cliente_numero_endereco', '<small class="form-text text-danger">', '</small>') ?>
+                            </div>
+                            <div class="col-md-6">
+                                <label>Complemento</label>
+                                <input type="text" class="form-control" name="cliente_complemento" placeholder="Informe o complemento" value="<?php echo set_value('cliente_complemento') ?>">
+                                <?php echo form_error('cliente_complemento', '<small class="form-text text-danger">', '</small>') ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-3">
+                            <div class="col-md-2">
+                                <label>Bairro</label>
+                                <input type="text" class="form-control" name="cliente_bairro" placeholder="Informe o bairro" value="<?php echo set_value('cliente_bairro'); ?>">
+                                <?php echo form_error('cliente_bairro', '<small class="form-text text-danger">', '</small>') ?>
+                            </div>
+                            <div class="col-md-2">
+                                <label>CEP</label>
+                                <input type="text" class="form-control cep" name="cliente_cep" placeholder="Informe o CEP" value="<?php echo set_value('cliente_cep'); ?>">
+                                <?php echo form_error('cliente_cep', '<small class="form-text text-danger">', '</small>') ?>
+                            </div>
+                            <div class="col-md-3">
+                                <label>Cidade</label>
+                                <input type="text" class="form-control" name="cliente_cidade" placeholder="Informe a cidade" value="<?php echo set_value('cliente_cidade'); ?>">
+                                <?php echo form_error('cliente_cidade', '<small class="form-text text-danger">', '</small>') ?>
+                            </div>
+                            <div class="col-md-2">
+                                <label>UF</label>
+                                <input type="text" class="form-control uf" name="cliente_estado" placeholder="UF" value="<?php echo set_value('cliente_estado'); ?>">
+                                <?php echo form_error('cliente_estado', '<small class="form-text text-danger">', '</small>') ?>
+                            </div>
+
+                        </div>
+                    </fieldset>
+
+                    <fieldset class='mt-4 border p-3'>
+                        <legend class='small'><i class="fas fa-user-cog"></i>&nbsp; Preferências</legend>
+
+                        <div class="form-group row mb-3">
+                            <div class="col-md-2">
+                                <label>Cliente ativo</label>
+                                <select name='cliente_ativo' class="form-control">
+                                    <option value='1'>Sim</option>
+                                    <option value='0'>Não</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-md-8">
+                                <label>Observações</label>
+                                <textarea class="form-control" name="cliente_obs" placeholder="Observações"><?php echo set_value('cliente_obs'); ?></textarea>
+                                <?php echo form_error('cliente_obs', '<small class="form-text text-danger">', '</small>') ?>
+                            </div>
+                        </div>
+                    </fieldset>
+
+                    <button type="submit" class="btn btn-success btn-sm ">Salvar</button>
+                    <a href='<?php echo base_url('clientes') ?>' title='Voltar' class='btn btn-primary btn-sm ml-2'>Voltar</a>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
