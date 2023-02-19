e4450a9e78c8a1f1706e5c4fba2b3a2b172c477b
Cleiton <cleiton_salvagni@estudante.sc.senai.br>
2023-02-19 19:14:07 -0300

    feat: add new Vendedor

--- application/controllers/Vendedores.php
+++ application/controllers/Vendedores.php
@@ -38,6 +38,73 @@ class Vendedores extends CI_Controller
         $this->load->view('layout/footer');
     }
 
+    public function novo()
+    {
+        $this->form_validation->set_rules('vendedor_nome_completo', '', 'trim|required|min_length[4]|max_length[145]');
+        $this->form_validation->set_rules('vendedor_cpf', '', 'trim|required|min_length[4]|max_length[25]|is_unique[vendedores.vendedor_cpf]|callback_check_cpf');
+        $this->form_validation->set_rules('vendedor_rg', '', 'trim|max_length[25]|is_unique[vendedores.vendedor_rg]');
+        $this->form_validation->set_rules('vendedor_email', '', 'trim|required|valid_email|max_length[45]|is_unique[vendedores.vendedor_email]');
+        $this->form_validation->set_rules('vendedor_celular', '', 'trim|required|max_length[15]|is_unique[vendedores.vendedor_celular]');
+
+        if ($this->input->post('vendedor_telefone')) {
+            $this->form_validation->set_rules('vendedor_telefone', '', 'trim|max_length[14]|callback_check_telefone');
+        }
+
+        $this->form_validation->set_rules('vendedor_cep', '', 'trim|required|exact_length[9]');
+        $this->form_validation->set_rules('vendedor_endereco', '', 'trim|required|max_length[45]');
+        $this->form_validation->set_rules('vendedor_numero_endereco', '', 'trim|max_length[25]');
+        $this->form_validation->set_rules('vendedor_bairro', '', 'trim|required|max_length[45]');
+        $this->form_validation->set_rules('vendedor_complemento', '', 'trim|max_length[145]');
+        $this->form_validation->set_rules('vendedor_cidade', '', 'trim|required|max_length[45]');
+        $this->form_validation->set_rules('vendedor_estado', '', 'trim|required|exact_length[2]');
+        $this->form_validation->set_rules('vendedor_obs', '', 'max_length[400]');
+
+
+        if ($this->form_validation->run()) {
+            $data = elements(
+                array(
+                    'vendedor_codigo',
+                    'vendedor_nome_completo',
+                    'vendedor_cpf',
+                    'vendedor_rg',
+                    'vendedor_email',
+                    'vendedor_celular',
+                    'vendedor_telefone',
+                    'vendedor_endereco',
+                    'vendedor_numero_endereco',
+                    'vendedor_complemento',
+                    'vendedor_bairro',
+                    'vendedor_cep',
+                    'vendedor_cidade',
+                    'vendedor_estado',
+                    'vendedor_ativo',
+                    'vendedor_obs',
+                ),
+                $this->input->post()
+            );
+
+            $data['vendedor_estado'] = strtoupper($this->input->post('vendedor_estado'));
+
+            $data = html_escape($data);
+
+            $this->core_model->insert('vendedores', $data);
+            redirect('vendedores');
+        } else {
+            $data = array(
+                'titulo' => 'Cadastrar Vendedor',
+                'scripts' => array(
+                    'vendor/mask/jquery.mask.min.js',
+                    'vendor/mask/app.js',
+                ),
+                'vendedor_codigo' => $this->core_model->generate_unique_code('vendedores', 'numeric', 8, 'vendedor_codigo'),
+            );
+
+            $this->load->view('layout/header', $data);
+            $this->load->view('vendedores/novo');
+            $this->load->view('layout/footer');
+        }
+    }
+
     public function edit($vendedor_id = NULL)
     {
 
--- application/views/vendedores/novo.php
+++ application/views/vendedores/novo.php
@@ -13,56 +13,51 @@
 
         <div class="card shadow mb-4">
             <div class="card-body">
-                <form method="POST" name="form_edit">
-
-                    <p><strong><i class="fas fa-clock"></i>&nbsp;&nbsp;Última alteração:&nbsp;</strong><?php echo formata_data_banco_com_hora($vendedor->vendedor_data_alteracao) ?></p>
-
+                <form method="POST" name="form_novo">
                     <fieldset class='mt-4 border p-3'>
                         <legend class='small'><i class="fas fa-user"></i>&nbsp; Dados Pessoais</legend>
-
                         <div class="form-group row mb-3">
                             <div class="col-md-2">
                                 <label>Matrícula</label>
-                                <input type="text" class="form-control" name="vendedor_codigo" placeholder="Matricula" readonly value="<?php echo $vendedor->vendedor_codigo; ?>">
-                                <?php echo form_error('vendedor_codigo', '<small class="form-text text-danger">', '</small>') ?>
+                                <input type="text" class="form-control" readonly value="<?php echo $vendedor_codigo ?>" name="vendedor_codigo">
                             </div>
 
                             <div class="col-md-4">
-                                <label>Nome Completo</label>
-                                <input type="text" class="form-control" name="vendedor_nome_completo" placeholder="Informe o nome completo" value="<?php echo $vendedor->vendedor_nome_completo; ?>">
+                                <label>Nome Completo <strong class='text-danger'>*</strong></label>
+                                <input type="text" class="form-control" name="vendedor_nome_completo" placeholder="Informe o nome completo" value="<?php echo set_value('vendedor_nome_completo') ?>">
                                 <?php echo form_error('vendedor_nome_completo', '<small class="form-text text-danger">', '</small>') ?>
 
                             </div>
 
                             <div class="col-md-3">
-                                <label>CPF</label>
-                                <input type="text" class="form-control cpf" name="vendedor_cpf" placeholder="Informe o CPF" value="<?php echo $vendedor->vendedor_cpf; ?>">
+                                <label>CPF <strong class='text-danger'>*</strong></label>
+                                <input type="text" class="form-control cpf" name="vendedor_cpf" placeholder="Informe o CPF" value="<?php echo set_value('vendedor_cpf') ?>">
                                 <?php echo form_error('vendedor_cpf', '<small class="form-text text-danger">', '</small>') ?>
                             </div>
 
                             <div class="col-md-3">
                                 <label>RG</label>
-                                <input type="text" class="form-control" name="vendedor_rg" placeholder="Informe o RG" value="<?php echo $vendedor->vendedor_rg; ?>">
+                                <input type="text" class="form-control" name="vendedor_rg" placeholder="Informe o RG" value="<?php echo set_value('vendedor_rg') ?>">
                                 <?php echo form_error('vendedor_rg', '<small class="form-text text-danger">', '</small>') ?>
                             </div>
                         </div>
 
                         <div class="form-group row mb-3">
                             <div class="col-md-3">
-                                <label>Email</label>
-                                <input type="email" class="form-control" name="vendedor_email" placeholder="Informe o email" value="<?php echo $vendedor->vendedor_email; ?>">
+                                <label>Email <strong class='text-danger'>*</strong></label>
+                                <input type="email" class="form-control" name="vendedor_email" placeholder="Informe o email" value="<?php echo set_value('vendedor_email') ?>">
                                 <?php echo form_error('vendedor_email', '<small class="form-text text-danger">', '</small>') ?>
                             </div>
 
                             <div class="col-md-3">
-                                <label>Celular</label>
-                                <input type="text" class="form-control sp_celphones" name="vendedor_celular" placeholder="Informe o celular" value="<?php echo $vendedor->vendedor_celular; ?>">
+                                <label>Celular <strong class='text-danger'>*</strong></label>
+                                <input type="text" class="form-control sp_celphones" name="vendedor_celular" placeholder="Informe o celular" value="<?php echo set_value('vendedor_celular') ?>">
                                 <?php echo form_error('vendedor_celular', '<small class="form-text text-danger">', '</small>') ?>
                             </div>
 
                             <div class="col-md-3">
                                 <label>Telefone fixo</label>
-                                <input type="text" class="form-control phone_with_ddd" name="vendedor_telefone" placeholder="Informe o telefone" value="<?php echo $vendedor->vendedor_telefone; ?>">
+                                <input type="text" class="form-control phone_with_ddd" name="vendedor_telefone" placeholder="Informe o telefone" value="<?php echo set_value('vendedor_telefone') ?>">
                                 <?php echo form_error('vendedor_telefone', '<small class="form-text text-danger">', '</small>') ?>
                             </div>
                         </div>
@@ -72,25 +67,25 @@
                         <legend class='small'><i class="fas fa-map-marker-alt"></i>&nbsp; Endereço</legend>
                         <div class="form-group row mb-3">
                             <div class="col-md-4">
-                                <label>Endereço</label>
-                                <input type="text" class="form-control" name="vendedor_endereco" placeholder="Informe o endereço" value="<?php echo $vendedor->vendedor_endereco; ?>">
+                                <label>Endereço <strong class='text-danger'>*</strong></label>
+                                <input type="text" class="form-control" name="vendedor_endereco" placeholder="Informe o endereço" value="<?php echo set_value('vendedor_endereco') ?>">
                                 <?php echo form_error('vendedor_endereco', '<small class="form-text text-danger">', '</small>') ?>
                             </div>
                             <div class="col-md-2">
                                 <label>Numero</label>
-                                <input type="text" class="form-control" name="vendedor_numero_endereco" placeholder="Informe o número" value="<?php echo $vendedor->vendedor_numero_endereco; ?>">
+                                <input type="text" class="form-control" name="vendedor_numero_endereco" placeholder="Informe o número" value="<?php echo set_value('vendedor_numero_endereco') ?>">
                                 <?php echo form_error('vendedor_numero_endereco', '<small class="form-text text-danger">', '</small>') ?>
                             </div>
 
                             <div class="col-md-3">
-                                <label>Bairro</label>
-                                <input type="text" class="form-control" name="vendedor_bairro" placeholder="Informe o bairro" value="<?php echo $vendedor->vendedor_bairro; ?>">
+                                <label>Bairro <strong class='text-danger'>*</strong></label>
+                                <input type="text" class="form-control" name="vendedor_bairro" placeholder="Informe o bairro" value="<?php echo set_value('vendedor_bairro') ?>">
                                 <?php echo form_error('vendedor_bairro', '<small class="form-text text-danger">', '</small>') ?>
                             </div>
 
                             <div class="col-md-3">
                                 <label>Complemento</label>
-                                <input type="text" class="form-control" name="vendedor_complemento" placeholder="Informe o complemento" value="<?php echo $vendedor->vendedor_complemento; ?>">
+                                <input type="text" class="form-control" name="vendedor_complemento" placeholder="Informe o complemento" value="<?php echo set_value('vendedor_complemento') ?>">
                                 <?php echo form_error('vendedor_complemento', '<small class="form-text text-danger">', '</small>') ?>
                             </div>
                         </div>
@@ -98,18 +93,18 @@
                         <div class="form-group row mb-3">
 
                             <div class="col-md-2">
-                                <label>CEP</label>
-                                <input type="text" class="form-control cep" name="vendedor_cep" placeholder="Informe o CEP" value="<?php echo $vendedor->vendedor_cep; ?>">
+                                <label>CEP <strong class='text-danger'>*</strong></label>
+                                <input type="text" class="form-control cep" name="vendedor_cep" placeholder="Informe o CEP" value="<?php echo set_value('vendedor_cep') ?>">
                                 <?php echo form_error('vendedor_cep', '<small class="form-text text-danger">', '</small>') ?>
                             </div>
                             <div class="col-md-4">
-                                <label>Cidade</label>
-                                <input type="text" class="form-control" name="vendedor_cidade" placeholder="Informe a cidade" value="<?php echo $vendedor->vendedor_cidade; ?>">
+                                <label>Cidade <strong class='text-danger'>*</strong></label>
+                                <input type="text" class="form-control" name="vendedor_cidade" placeholder="Informe a cidade" value="<?php echo set_value('vendedor_cidade') ?>">
                                 <?php echo form_error('vendedor_cidade', '<small class="form-text text-danger">', '</small>') ?>
                             </div>
                             <div class="col-md-2">
-                                <label>UF</label>
-                                <input type="text" class="form-control uf" name="vendedor_estado" placeholder="UF" value="<?php echo $vendedor->vendedor_estado; ?>">
+                                <label>UF <strong class='text-danger'>*</strong></label>
+                                <input type="text" class="form-control uf" name="vendedor_estado" placeholder="UF" value="<?php echo set_value('vendedor_estado') ?>">
                                 <?php echo form_error('vendedor_estado', '<small class="form-text text-danger">', '</small>') ?>
                             </div>
                         </div>
@@ -122,8 +117,8 @@
                             <div class="col-md-2">
                                 <label>Vendedor ativo</label>
                                 <select name='vendedor_ativo' class="form-control">
-                                    <option value='1' <?php echo ($vendedor->vendedor_ativo == 1 ? 'selected' : '') ?>>Sim</option>
-                                    <option value='0' <?php echo ($vendedor->vendedor_ativo == 0 ? 'selected' : '') ?>>Não</option>
+                                    <option value='1'>Sim</option>
+                                    <option value='0'>Não</option>
                                 </select>
                             </div>
                         </div>
@@ -131,13 +126,10 @@
                     <div class="form-group row">
                         <div class="col-md-12">
                             <label>Observações</label>
-                            <textarea class="form-control" name="vendedor_obs" placeholder="Observações"><?php echo $vendedor->vendedor_obs ?></textarea>
+                            <textarea class="form-control" name="vendedor_obs" placeholder="Observações"><?php echo set_value('vendedor_obs') ?></textarea>
                             <?php echo form_error('vendedor_obs', '<small class="form-text text-danger">', '</small>') ?>
                         </div>
                     </div>
-
-
-                    <input type='hidden' name='vendedor_id' value="<?php echo $vendedor->vendedor_id; ?>">
 
                     <button type="submit" class="btn btn-success btn-sm ">Salvar</button>
                     <a href='<?php echo base_url($this->router->fetch_class()) ?>' title='Voltar' class='btn btn-primary btn-sm ml-2'>Voltar</a>
