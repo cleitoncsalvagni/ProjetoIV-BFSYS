ca8fa6007c743e88c56a1ace6d6ddddaaf9427fe
Cleiton Salvagni <cleiton_salvagni@estudante.sc.senai.br>
2023-02-24 11:01:04 -0300

    feat: toastr

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -11,13 +11,36 @@
 
   <?php echo (isset($titulo) ? '<title>BFSYS | ' . $titulo . '</title>' : '<title>BFSYS</title>') ?>
 
-
   <link href="<?php echo base_url("public/vendor/fontawesome-free/css/all.min.css") ?>" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-
-  <!-- <link href="<?php echo base_url("public/css/app.css"); ?>" rel="stylesheet"> -->
   <link href="<?php echo base_url("public/css/sb-admin-2.min.css"); ?>" rel="stylesheet">
+
+  <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" rel="stylesheet" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.js.map"></script>
+
+  <script>
+    toastr.options = {
+      closeButton: true,
+      debug: false,
+      newestOnTop: false,
+      progressBar: true,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+      onclick: null,
+      showDuration: 300,
+      hideDuration: 1000,
+      timeOut: 3000,
+      extendedTimeOut: 1000,
+      showEasing: 'swing',
+      hideEasing: 'linear',
+      showMethod: 'fadeIn',
+      hideMethod: 'fadeOut',
+    }
+  </script>
+
 
   <?php if (isset($styles)) : ?>
 
@@ -32,6 +55,5 @@
 </head>
 
 <body id="page-top">
-
 
   <div id="wrapper">
