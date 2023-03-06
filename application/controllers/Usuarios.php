20ba8ec636cf2b233e7c710d682096d2c18e556f
Cleiton Salvagni <cleiton_salvagni@estudante.sc.senai.br>
2023-03-06 15:30:25 -0300

    refactor: min length password

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -31,11 +31,6 @@ class Usuarios extends CI_Controller
 
         );
 
-        // echo '<pre>';
-        // print_r($data['usuarios']);
-        // exit();
-
-
         $this->load->view('layout/header', $data);
         $this->load->view('usuarios/index');
         $this->load->view('layout/footer');
@@ -48,7 +43,7 @@ class Usuarios extends CI_Controller
         $this->form_validation->set_rules('last_name', '', 'trim|required');
         $this->form_validation->set_rules('email', '', 'trim|required|valid_email|is_unique[users.email]');
         $this->form_validation->set_rules('username', '', 'trim|required|is_unique[users.username]');
-        $this->form_validation->set_rules('password', '', 'required|min_length[8]|max_length[50]');
+        $this->form_validation->set_rules('password', '', 'required|min_length[3]|max_length[50]');
         $this->form_validation->set_rules('confirm_password', '', 'matches[password]');
 
         if ($this->form_validation->run()) {
@@ -124,30 +119,11 @@ class Usuarios extends CI_Controller
                 'perfil_usuario' => $this->ion_auth->get_users_groups($user_id)->row(),
             );
 
-            /* 
-            
-            [first_name] => Admin
-            [last_name] => istrator
-            [email] => admin@admin.com
-            [username] => administrator
-            [active] => 1
-            [perfil_usuario] => 1
-            [password] => 
-            [confirm_password] => 
-            [usuario_id] => 1
-
-            */
-
-            // echo '<pre>';
-            // print_r($this->input->post());
-            // exit();
-
-
             $this->form_validation->set_rules('first_name', '', 'trim|required');
             $this->form_validation->set_rules('last_name', '', 'trim|required');
             $this->form_validation->set_rules('email', '', 'trim|required|valid_email|callback_email_check');
             $this->form_validation->set_rules('username', '', 'trim|required|callback_username_check');
-            $this->form_validation->set_rules('password', '', 'min_length[8]|max_length[50]');
+            $this->form_validation->set_rules('password', '', 'min_length[3]|max_length[50]');
             $this->form_validation->set_rules('confirm_password', '', 'matches[password]');
 
             if ($this->form_validation->run()) {
