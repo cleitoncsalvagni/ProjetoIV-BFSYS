ed7d01d6dc8a5abe083e315edc5a7b4178f186eb
Cleiton Salvagni <cleiton_salvagni@estudante.sc.senai.br>
2023-03-13 12:58:17 -0300

    feat: config. central notificações

--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -5,81 +5,157 @@
     <i class="fa fa-bars"></i>
   </button>
 
-
   <ul class="navbar-nav ml-auto">
-    <li class="nav-item dropdown no-arrow mx-1">
-      <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="fas fa-bell fa-fw"></i>
-        <span class="badge badge-danger badge-counter">3+</span>
+
+
+    <li class="nav-item dropdown no-arrow d-sm-none">
+      <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-search fa-fw"></i>
       </a>
-      <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
-        <h6 class="dropdown-header">
-          Alerts Center
-        </h6>
-        <a class="dropdown-item d-flex align-items-center" href="#">
-          <div class="mr-3">
-            <div class="icon-circle bg-primary">
-              <i class="fas fa-file-alt text-white"></i>
+
+      <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+        <form class="form-inline mr-auto w-100 navbar-search">
+          <div class="input-group">
+            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+            <div class="input-group-append">
+              <button class="btn btn-primary" type="button">
+                <i class="fas fa-search fa-sm"></i>
+              </button>
             </div>
           </div>
-          <div>
-            <div class="small text-gray-500">December 12, 2019</div>
-            <span class="font-weight-bold">A new monthly report is ready to download!</span>
-          </div>
-        </a>
-        <a class="dropdown-item d-flex align-items-center" href="#">
-          <div class="mr-3">
-            <div class="icon-circle bg-success">
-              <i class="fas fa-donate text-white"></i>
-            </div>
-          </div>
-          <div>
-            <div class="small text-gray-500">December 7, 2019</div>
-            $290.29 has been deposited into your account!
-          </div>
-        </a>
-        <a class="dropdown-item d-flex align-items-center" href="#">
-          <div class="mr-3">
-            <div class="icon-circle bg-warning">
-              <i class="fas fa-exclamation-triangle text-white"></i>
-            </div>
-          </div>
-          <div>
-            <div class="small text-gray-500">December 2, 2019</div>
-            Spending Alert: We've noticed unusually high spending for your account.
-          </div>
-        </a>
-        <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+        </form>
       </div>
     </li>
 
+    <?php if ($this->ion_auth->is_admin()) : ?>
+
+
+      <?php if (isset($contador_notificacoes) && $contador_notificacoes > 0) : ?>
+
+        <li class="nav-item dropdown no-arrow mx-1">
+          <a class="nav-link dropdown-toggle blink_me" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-bell fa-fw text-gray-900"></i>
+
+            <span class="badge badge-danger badge-counter"><?php echo $contador_notificacoes; ?></span>
+          </a>
+
+          <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
+            <h6 class="dropdown-header">
+              CENTRAL DE NOTIFICAÇÕES
+            </h6>
+
+            <?php if ($contas_receber_vencidas) : ?>
+
+              <a title="Gerenciar contas a receber" class="dropdown-item d-flex align-items-center" href="<?php echo base_url('receber'); ?>">
+                <div class="mr-3">
+                  <div class="icon-circle bg-danger">
+                    <i class="fas fa-hand-holding-usd fa-lg text-white"></i>
+                  </div>
+                </div>
+                <div>
+                  <div class="small text-gray-500"><?php echo formata_data_banco_sem_hora(date('Y-m-d')); ?></div>
+                  <span class="font-weight-bold">Existem contas a receber vencidas!</span>
+                </div>
+              </a>
+            <?php endif; ?>
+
+            <?php if ($contas_pagar_vencidas) : ?>
+
+              <a title="Gerenciar contas a pagar" class="dropdown-item d-flex align-items-center" href="<?php echo base_url('pagar'); ?>">
+                <div class="mr-3">
+                  <div class="icon-circle bg-danger">
+                    <i class="fas fa-money-bill-alt fa-lg text-white"></i>
+                  </div>
+                </div>
+                <div>
+                  <div class="small text-gray-500"><?php echo formata_data_banco_sem_hora(date('Y-m-d')); ?></div>
+                  <span class="font-weight-bold">Existem contas a pagar vencidas!</span>
+                </div>
+              </a>
+            <?php endif; ?>
+
+            <?php if ($contas_pagar_vencem_hoje) : ?>
+
+              <a title="Gerenciar contas a pagar" class="dropdown-item d-flex align-items-center" href="<?php echo base_url('pagar'); ?>">
+                <div class="mr-3">
+                  <div class="icon-circle bg-warning">
+                    <i class="fas fa-money-bill-alt fa-lg text-white"></i>
+                  </div>
+                </div>
+                <div>
+                  <div class="small text-gray-500"><?php echo formata_data_banco_sem_hora(date('Y-m-d')); ?></div>
+                  <span class="font-weight-bold">Existem contas a pagar que vencem hoje!</span>
+                </div>
+              </a>
+            <?php endif; ?>
+
+            <?php if ($contas_receber_vencem_hoje) : ?>
+
+              <a title="Gerenciar contas a pagar" class="dropdown-item d-flex align-items-center" href="<?php echo base_url('receber'); ?>">
+                <div class="mr-3">
+                  <div class="icon-circle bg-warning">
+                    <i class="fas fa-hand-holding-usd fa-lg text-white"></i>
+                  </div>
+                </div>
+                <div>
+                  <div class="small text-gray-500"><?php echo formata_data_banco_sem_hora(date('Y-m-d')); ?></div>
+                  <span class="font-weight-bold">Existem contas a receber que vencem hoje!</span>
+                </div>
+              </a>
+            <?php endif; ?>
+
+
+
+            <?php if ($usuarios_desativados) : ?>
+
+              <a title="Gerenciar usuários" class="dropdown-item d-flex align-items-center" href="<?php echo base_url('usuarios'); ?>">
+                <div class="mr-3">
+                  <div class="icon-circle bg-info">
+                    <i class="fas fa-users fa-lg text-white"></i>
+                  </div>
+                </div>
+                <div>
+                  <div class="small text-gray-500"><?php echo formata_data_banco_sem_hora(date('Y-m-d')); ?></div>
+                  <span class="font-weight-bold">Existem usuários desativados!</span>
+                </div>
+              </a>
+            <?php endif; ?>
+
+
+
+          </div>
+
+        </li>
+
+      <?php endif; ?>
+    <?php endif; ?>
+
+
     <div class="topbar-divider d-none d-sm-block"></div>
+
 
     <li class="nav-item dropdown no-arrow">
       <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <span class="mr-2 d-none d-lg-inline text-gray-600 small">User</span>
-        <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+
+        <?php $user = $this->ion_auth->user()->row(); ?>
+
+        <span class="mr-2 d-none d-lg-inline text-gray-900"><?php echo $user->first_name; ?> </span>
+        <img class="img-profile rounded-circle" src="<?php echo base_url('public/img/bfsys_logo_icon.svg') ?>">
       </a>
+
       <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-        <a class="dropdown-item" href="<?php echo base_url("usuarios/edit/" . $this->session->userdata('user_id')) ?>">
-          <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-700"></i>
+        <a class="dropdown-item" href="<?php echo base_url('usuarios/edit/' . $this->session->userdata('user_id')); ?>">
+          <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-900"></i>
           Perfil
         </a>
-        <!-- <a class="dropdown-item" href="#">
-          <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-700"></i>
-          Settings
-        </a>
-        <a class="dropdown-item" href="#">
-          <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-700"></i>
-          Activity Log
-        </a> -->
         <div class="dropdown-divider"></div>
         <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-          <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-700"></i>
+          <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-900"></i>
           Sair
         </a>
       </div>
     </li>
 
   </ul>
+
 </nav>
