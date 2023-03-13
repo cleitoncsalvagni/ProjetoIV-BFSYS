4012cb02fbd0945424924c083c2d25c595bcf0eb
Cleiton Salvagni <cleiton_salvagni@estudante.sc.senai.br>
2023-03-13 12:58:36 -0300

    fix: ajusts home model

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -7,6 +7,7 @@ class Home_model extends CI_Model
 
     public function get_sum_vendas()
     {
+
         $this->db->select([
             'FORMAT(SUM(REPLACE(venda_valor_total, ",", "")), 2) as venda_valor_total',
         ]);
@@ -16,6 +17,7 @@ class Home_model extends CI_Model
 
     public function get_sum_os()
     {
+
         $this->db->select([
             'FORMAT(SUM(REPLACE(ordem_servico_valor_total, ",", "")), 2) as os_valor_total',
         ]);
@@ -25,6 +27,7 @@ class Home_model extends CI_Model
 
     public function get_sum_receber()
     {
+
         $this->db->select([
             'FORMAT(SUM(REPLACE(conta_receber_valor, ",", "")), 2) as conta_receber_valor_total',
         ]);
@@ -36,6 +39,7 @@ class Home_model extends CI_Model
 
     public function get_sum_pagar()
     {
+
         $this->db->select([
             'FORMAT(SUM(REPLACE(conta_pagar_valor, ",", "")), 2) as conta_pagar_valor_total',
         ]);
@@ -47,21 +51,23 @@ class Home_model extends CI_Model
 
     public function get_contas_pagar_venc_hoje()
     {
+
         $this->db->select([
             'contas_pagar.*',
             'fornecedor_id',
-            'fornecedor_nome_fantasia as fornecedor'
+            'fornecedor_nome_fantasia as fornecedor',
         ]);
 
         $this->db->where('conta_pagar_status', 0);
         $this->db->where('conta_pagar_data_vencimento =', date('Y-m-d'));
 
         $this->db->join('fornecedores', 'fornecedor_id = conta_pagar_fornecedor_id', 'LEFT');
-        return $this->db->get('contas_pagar')->result();
+        return $this->db->get('contas_pagar')->row();
     }
 
     public function get_contas_receber_venc_hoje()
     {
+
         $this->db->select([
             'contas_receber.*',
             'cliente_id',
@@ -72,20 +78,22 @@ class Home_model extends CI_Model
         $this->db->where('conta_receber_data_vencimento =', date('Y-m-d'));
 
         $this->db->join('clientes', 'cliente_id = conta_receber_cliente_id', 'LEFT');
-        return $this->db->get('contas_receber')->result();
+        return $this->db->get('contas_receber')->row();
     }
 
     public function get_produtos_mais_vendidos()
     {
+
         $this->db->select([
             'venda_produtos.*',
             'SUM(venda_produto_quantidade) as quantidade_vendidos',
             'produtos.produto_id',
-            'produtos.produto_descricao'
+            'produtos.produto_descricao',
         ]);
 
         $this->db->join('produtos', 'produto_id = venda_produto_id_produto', 'LEFT');
         $this->db->limit(3);
+
         $this->db->group_by('venda_produto_id_produto');
         $this->db->order_by('quantidade_vendidos', 'DESC');
 
@@ -94,15 +102,17 @@ class Home_model extends CI_Model
 
     public function get_servicos_mais_vendidos()
     {
+
         $this->db->select([
             'ordem_tem_servicos.*',
             'SUM(ordem_ts_quantidade) as quantidade_vendidos',
             'servicos.servico_id',
-            'servicos.servico_nome'
+            'servicos.servico_nome',
         ]);
 
-        $this->db->join('servicos', 'servico_id = ordem_ts_id_servico ', 'LEFT');
+        $this->db->join('servicos', 'servico_id = ordem_ts_id_servico', 'LEFT');
         $this->db->limit(3);
+
         $this->db->group_by('ordem_ts_id_servico');
         $this->db->order_by('quantidade_vendidos', 'DESC');
 
@@ -111,23 +121,26 @@ class Home_model extends CI_Model
 
     public function get_usuarios_desativados()
     {
+
         $this->db->where('active', 0);
-        $this->db->get('users')->row();
+        return $this->db->get('users')->row();
     }
 
     public function get_receber_vencidas()
     {
+
         $this->db->where('conta_receber_data_vencimento <', date('Y-m-d'));
         $this->db->where('conta_receber_status', 0);
 
-        $this->db->get('contas_receber')->row();
+        return $this->db->get('contas_receber')->row();
     }
 
     public function get_pagar_vencidas()
     {
+
         $this->db->where('conta_pagar_data_vencimento <', date('Y-m-d'));
         $this->db->where('conta_pagar_status', 0);
 
-        $this->db->get('contas_pagar')->row();
+        return $this->db->get('contas_pagar')->row();
     }
 }
